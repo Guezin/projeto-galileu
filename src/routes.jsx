f35a5412a7a4d16e4e3e08aa6e3f9b206819d2a4
Guezin <leandroguezinjunior@gmail.com>
2020-07-16 17:47:47 -0300

    deploy

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,13 +4,17 @@ import { Route, BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+import Home from "./pages/Home";
+
 const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Route component={Login} path="/" exact />
-            <Route component={Register} path="/new" />
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Route component={Login} path="/" exact />
+      <Route component={Register} path="/new" />
+
+      <Route path="/home" component={Home} />
+    </BrowserRouter>
+  );
 };
 
 export default Routes;
