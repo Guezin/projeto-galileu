2724adef1f4a944b7a44e443e2dd4adb29016542
deboralbarros <deboralbarros@users.noreply.github.com>
2020-07-16 12:07:58 -0400

    inserindo animações

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,39 +1,44 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import "./styles.css";
+import './styles.css';
+import { motion } from 'framer-motion';
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-  return (
-    <div className="login-container">
-      <section className="form">
-          <p>Faça o seu Login</p>
-          <form>
-              <input 
-                type="text" 
-                placeholder="E-mail" 
-                value={email} 
-                onChange={e => setEmail(e.target.value)} 
-              />
+	return (
+		<motion.div initial={{ x: '100%' }} animate={{ x: 'calc(100vw - 100%)' }} transition={{ duration: 1 }}>
+			<div className="login-container">
+				<section className="form">
+					<p>Faça o seu Login</p>
+					<form>
+						<input
+							type="text"
+							placeholder="E-mail"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
 
-              <input 
-                type="password" 
-                placeholder="Senha" 
-                value={password} 
-                onChange={e => setPassword(e.target.value)} 
-              />
-              
-              <div className="button-options">
-                <button className="button">Entrar</button>
-                <Link className="button" to="/new" >Cadastrar</Link>
-              </div>
-          </form>
-      </section>
-    </div>
-  );
+						<input
+							type="password"
+							placeholder="Senha"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+
+						<div className="button-options">
+							<button className="button">Entrar</button>
+							<Link className="button" to="/new">
+								Cadastrar
+							</Link>
+						</div>
+					</form>
+				</section>
+			</div>
+		</motion.div>
+	);
 };
 
 export default Login;
